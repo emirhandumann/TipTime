069efda7cbd678c50bb4594c7eb221071d0bf8bd
Emirhan <emirhanduman36@hotmail.com>
2023-03-26 15:17:40 +0300

    Project is completed.

--- app/src/main/java/com/emirhanduman/tiptime/MainActivity.kt
+++ app/src/main/java/com/emirhanduman/tiptime/MainActivity.kt
@@ -16,26 +16,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.calculateButton.setOnClickListener{calculate()}
+        binding.calculateButton.setOnClickListener { calculate() }
 
 
     }
 
-    fun calculate(){
+    fun calculate() {
         val cost = binding.costOfService.text.toString().toDouble()
         val selectedId = binding.tipOptions.checkedRadioButtonId
 
-        val tipPercentage = when(selectedId){
+        val tipPercentage = when (selectedId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
         }
 
-        var tip = cost*tipPercentage
+        var tip = cost * tipPercentage
 
         val roundUp = binding.roundUpSwitch.isChecked
 
-        if (roundUp){
+        if (roundUp) {
             tip = kotlin.math.ceil(tip)
         }
 
@@ -43,9 +43,9 @@ class MainActivity : AppCompatActivity() {
 
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
 
-        binding.tipResult.text = getString(R.string.tip_amount,formattedTip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
 
-        val toast = Toast.makeText(this,"Thanks for tip!",Toast.LENGTH_LONG)
+        val toast = Toast.makeText(this, "Thanks for tip!", Toast.LENGTH_LONG)
         toast.show()
 
     }
